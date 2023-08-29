5e7d600462f2ce70faeb09f9fd0e2b23d12a3776
Balasubramanian S <68114029+balasubramanian-s@users.noreply.github.com>
2023-08-30 01:54:17 +0530

    Update example.rb

--- infra-audi/controls/example.rb
+++ infra-audi/controls/example.rb
@@ -26,24 +26,7 @@ control 'aws-s3-buckets-1.0' do
 end
 #ec2
 
-control 'aws-ec2-capacity-reservations-1.0' do
-  title 'Describes one or more of your Capacity Reservations.'
 
-  describe aws_ec2_capacity_reservations do
-    it { should exist }
-  end
-
-  describe aws_ec2_capacity_reservations do
-    its('tenancies') { should_not include 'default' }
-    its('total_instance_counts') { should include 1 }
-    its('ebs_optimized') { should include false }
-    its('ephemeral_storages') { should include false }
-    its('states') { should include 'active' }
-    its('end_date_types') { should include 'unlimited' }
-    its('instance_match_criterias') { should include 'open' }
-    its('tags') { should_not be_empty }
-  end
-end
 
 control "aws-vpcs-multi-region-status-check" do                             # A unique ID for this control.
   impact 1.0                                                                # The criticality, if this control fails.
